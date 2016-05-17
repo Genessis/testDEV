e2a09220fbe5904af17eb20aa45017eab0c83911
Genessis Sanchez <genny@genny.ac.labf.usb.ve>
2016-05-16 20:08:42 -0430

    Django funcionando

---
+++ testSite/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'home/index.html')
+
+def search(request):
+    return render(request, 'home/search.html')
+# Create your views here.
